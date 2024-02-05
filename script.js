0da8859d5f65d4db6ff6d104bb472ac0965c2268
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-06 00:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-//your code here
-
-// console.log(itemPrice)
-
 let btn =  document.createElement('button');
 btn.append("Get Sum");
 document.body.appendChild(btn);
 
-
 function getSum(){
-let itemPrice = document.querySelectorAll(".price");
-let sum =0;
-itemPrice.forEach((data)=>{
-	sum = sum + Number(data.innerHTML)
+let prices= document.querySelectorAll(".price");
+let grandTotal =0;
+prices.forEach((data)=>{
+	grandTotal +=  Number(data.innerHTML)
 })
 let tr = document.createElement('tr');
 let tdTotal = document.createElement('td');
-tdTotal.innerHTML = `Total Price (in Rs): ${sum}`;
+tdTotal.innerHTML = `Total Price (in Rs): ${grandTotal}`;
 tr.appendChild(tdTotal);
 document.querySelector("tbody").appendChild(tr);
-
 }
-
-
 
 btn.addEventListener('click',getSum)
