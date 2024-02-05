af641a5ff54e6152ac591d00bb82a8550341673a
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-06 00:19:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+let itemPrice = document.querySelectorAll(".price");
+// console.log(itemPrice)
+
+let btn =  document.createElement('button');
+btn.append("Get Sum");
+document.body.appendChild(btn);
+
+
+function getSum(){
+let sum = 0;
+itemPrice.forEach((data)=>{
+	sum = sum + Number(data.innerHTML)
+})
+
+let td = document.createElement('td');
+td.innerHTML = `Total Price (in Rs): ${sum}`;
+	document.querySelector(".total").appendChild(td)
+console.log(td);
+}
+
+
+btn.addEventListener('click',getSum)
+
+getSum();
