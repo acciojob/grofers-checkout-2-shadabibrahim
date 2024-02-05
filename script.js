5d0fd2d37efed0b8a304fc9647e2c3105ae8286f
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-06 00:57:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ btn.append("Get Sum");
 document.body.appendChild(btn);
 
 function getSum(){
-let prices= document.querySelectorAll(".price");
+let prices= document.querySelectorAll(".prices");
 let grandTotal =0;
-prices.forEach((data)=>{
-	grandTotal +=  Number(data.innerHTML)
+prices.forEach((price)=>{
+	grandTotal +=  Number(price.innerHTML)
 })
 let tr = document.createElement('tr');
 let tdTotal = document.createElement('td');
 tdTotal.innerHTML = `Total Price (in Rs): ${grandTotal}`;
 tr.appendChild(tdTotal);
+	document.querySelector("tbody").setAttribute('data-ns-test', 'grandTotal');
 document.querySelector("tbody").appendChild(tr);
 }
 
