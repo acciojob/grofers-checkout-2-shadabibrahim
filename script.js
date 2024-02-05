a6395cd0f5ef70ba56043d8afa39b8253dd36352
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-06 01:14:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-// let btn =  document.createElement('button');
-// btn.append("Get Sum");
-// document.body.appendChild(btn);
+let btn =  document.createElement('button');
+btn.append("Get Sum");
+document.body.appendChild(btn);
 
-// function getSum(){
-// let prices= document.querySelectorAll(".price");
-// let grandTotal =0;
-// prices.forEach((price)=>{
-// 	grandTotal +=  Number(price.innerHTML)
-// })
-// let tr = document.createElement('tr');
-// let tdTotal = document.createElement('td');
-// tdTotal.innerHTML = `Total Price (in Rs): ${grandTotal}`;
-// tr.appendChild(tdTotal);
-// document.querySelector("tbody").appendChild(tr);
-// }
+function getSum(){
+let prices= document.querySelectorAll('[data-ns-test="price"]');
+let grandTotal =0;
+prices.forEach((price)=>{
+	grandTotal +=  parseFloat(price.innerHTML)
+})
+let tr = document.createElement('tr');
+let tdTotal = document.createElement('td');
+	tdTotal.setAttribute('data-ns-test', 'grandTotal');
+tdTotal.innerHTML = `Total Price (in Rs): ${grandTotal.toFixed(2)}`;
+tr.appendChild(tdTotal);
+document.querySelector("tbody").appendChild(tr);
+}
 
-// btn.addEventListener('click',getSum)
+btn.addEventListener('click',getSum)
 
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-  getSumBtn.disabled = true;
-  const prices = document.querySelectorAll(".price");
-  let totalPrice = 0;
-  prices.forEach(price => {
-    const value = parseInt(price.textContent);
-    if(!Number.isNaN(value)){
-      totalPrice += value;
-    }
-  });
-  const totalPriceRow = document.createElement("tr");
-  totalPriceRow.id = "ans";
-  const totalPriceData = document.createElement("td");
-  totalPriceRow.appendChild(totalPriceData);
-  const data = `Total Price (in Rs): ${totalPrice}`;
-  totalPriceData.append(data);
-  const table = document.querySelector("tbody");
-  table.appendChild(totalPriceRow);
-};
-
-getSumBtn.addEventListener("click", getSum);
